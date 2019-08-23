5e020759e3f0e1c398fe8a5734e64ec3165e0109
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-23 10:26:37 -0500

    Added auth endpoints

--- api/server.js
+++ api/server.js
@@ -2,17 +2,43 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const session = require('express-session');
+const SessionStore = require('connect-session-knex')(session);
+
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+
+const sessionConfig = {
+	name: 'bipolarbear',
+	secret: 'hey guys im a secret do not tell the others',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: 60 * 60 * 1000,
+		secure: false,
+		httpOnly: true
+	},
+	store: new SessionStore({
+		knex: require('../database/dbConfig'),
+		tablename: 'sessions',
+		sidfieldname: 'sid',
+		createtable: true,
+		clearInterval: 60 * 60 * 1000
+	})
+};
+
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
-
+server.use('/api/users', usersRouter);
 module.exports = server;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require('./api/server.js');
+const server = require('./api/server');
 
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 5555;
+
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log(`//================Server is running on ${PORT}... =================//`);
 });
